6747c2d7805dc014ff13ff08425dc97576c9b4cc
JohnPaul-Londono <johnplondono@gmail.com>
2022-01-27 20:18:38 -0600

    added deleted

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 const ProductList = (props) => {
     const [products, setProducts] = useState([]);
 
+
     useEffect(() => {
         axios.get("http://localhost:8000/api/products")
             .then(res => {
@@ -17,13 +18,29 @@ const ProductList = (props) => {
             })
     }, [])
 
+    const onDeleteHandler = (_id, index) => {
+        // console.log(_id);
+        // console.log(index);
+        axios.delete(`http://localhost:8000/api/products/delete/${_id}`)
+        .then(res=>{
+            console.log(res);
+            const copyProducts = [...products];
+            copyProducts.splice(index, 1);
+            setProducts(copyProducts);
+        })
+        .catch(err=>console.log(err))
+    }
+
+
+
     return (
         <div>
             <h2>All Products</h2>
             {
                 products.map((product, i )=>{
                     return <li key={i}><Link to={`/api/products/one/${product._id}`}> {product.title}</Link>
-                    <Link to={`/api/products/edit/${product._id}`}><button>Edit</button></Link></li>
+                    <Link to={`/api/products/edit/${product._id}`}><button>Edit</button></Link>
+                    <button onClick={()=>onDeleteHandler(product._id, i)}>X</button></li>
                 })
             }
         </div>
--- client/src/views/ShowOne.js
+++ client/src/views/ShowOne.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 const ShowOne = (props) => {
     const { _id } = useParams();
     const [showone, setShowone] = useState([]);
+    const history = useHistory();
 
     useEffect(() => {
         axios.get("http://localhost:8000/api/products/one/" + _id)
@@ -19,11 +21,24 @@ const ShowOne = (props) => {
     }, [_id])
 
 
+
+    const onDeleteHandler = (_id, index) => {
+        // console.log(_id);
+        // console.log(index);
+        axios.delete(`http://localhost:8000/api/products/delete/${_id}`)
+        .then(res=>{
+            console.log(res);
+            history.push("/")
+        })
+        .catch(err=>console.log(err))
+    }
+
     return (
         <div>
             <p>Title: {showone.title}</p>
             <p>Price: {showone.price}</p>
             <p>Description: {showone.description}</p>
+            <button onClick={()=>onDeleteHandler(showone._id)}>Delete</button>
         </div>
     )
 }
